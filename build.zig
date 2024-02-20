fb78b9e2e30911d64ba60656975dfad89c7f9960
brian.orwe <brian.orwe@gmail.com>
2024-02-20 23:52:12 +0300

    gen-proto can now generate protobuf zig files

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const protobuf = @import("protobuf");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -15,6 +16,8 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const protobuf_dep = b.dependency("protobuf", .{ .target = target, .optimize = optimize });
+
     const lib = b.addStaticLibrary(.{
         .name = "zellig",
         // In this case the main source file is merely a path, however, in more
@@ -24,45 +27,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    lib.root_module.addImport("protobuf", protobuf_dep.module("protobuf"));
+
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
     b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "zellig",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
@@ -74,18 +44,28 @@ pub fn build(b: *std.Build) void {
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+
+    //add protobuf generation
+    const gen_step = b.step("gen-proto", "Generate Protobuf");
+    const proto_step = protobuf.RunProtocStep.create(b, protobuf_dep.builder, target, .{ .destination_directory = .{ .path = "src/proto" }, .source_files = &.{
+        "resize.proto",
+        "input_mode.proto",
+        "action.proto",
+        "command.proto",
+        "event.proto",
+        "file.proto",
+        "key.proto",
+        "message.proto",
+        "pipe_message.proto",
+        "plugin_command.proto",
+        "plugin_ids.proto",
+        "plugin_permission.proto",
+        "style.proto",
+    }, .include_directories = &.{"proto/"} });
+    gen_step.dependOn(&proto_step.step);
 }
